5e686a6cb6d04dbe0118805882626bec62041c16
César Martinez <cesar_martinez22@hotmail.com>
2020-06-12 16:58:25 -0300

    global interceptor

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Observable } from 'rxjs';
-
-import { OnCloseInterceptor } from './interceptors/on-close.interceptor';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('long-request')
-  @UseInterceptors(OnCloseInterceptor)
   longRequest(): Observable<number> {
     return this.appService.getLongRequest();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OnCloseInterceptor } from './interceptors/on-close.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Register a global interceptor
-  // app.useGlobalInterceptors(new OnCloseInterceptor());
+  app.useGlobalInterceptors(new OnCloseInterceptor());
   app.enableCors();
   await app.listen(3000);
 }
